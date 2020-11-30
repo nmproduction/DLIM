906ee9cd0cb429bdb5e280644006a1668eb0ec9f
nmproduction <36153988+nmproduction@users.noreply.github.com>
2020-11-30 12:31:57 +0100

    Update pix2pix_model.py

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -1,6 +1,7 @@
 import torch
 from .base_model import BaseModel
 from . import networks
+from . import sesamDis
 
 
 class Pix2PixModel(BaseModel):
@@ -57,8 +58,12 @@ class Pix2PixModel(BaseModel):
                                       not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
 
         if self.isTrain:  # define a discriminator; conditional GANs need to take both input and output images; Therefore, #channels for D is input_nc + output_nc
-            self.netD = networks.define_D(opt.input_nc + opt.output_nc, opt.ndf, opt.netD,
+            if opt.netD != 'sesam':
+                self.netD = networks.define_D(opt.input_nc + opt.output_nc, opt.ndf, opt.netD,
                                           opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
+            else:
+               self.netD = sesamDis.define_D(opt) 
+               self.multiscale = True
 
         if self.isTrain:
             # define loss functions
