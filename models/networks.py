9c532e17f3041b9e4283bd8a46c41a792f945702
nmproduction <36153988+nmproduction@users.noreply.github.com>
2020-12-08 14:21:30 +0100

    Update networks.py

--- models/networks.py
+++ models/networks.py
@@ -453,7 +453,7 @@ class ResnetBlock(nn.Module):
 
         conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias), norm_layer(dim), nn.ReLU(True)]
         if use_dropout:
-            conv_block += [nn.Dropout(0.95)]
+            conv_block += [nn.Dropout(0.5)]
 
         p = 0
         if padding_type == 'reflect':
@@ -563,7 +563,7 @@ class UnetSkipConnectionBlock(nn.Module):
             up = [uprelu, upconv, upnorm]
 
             if use_dropout:
-                model = down + [submodule] + up + [nn.Dropout(0.95)]
+                model = down + [submodule] + up + [nn.Dropout(0.5)]
             else:
                 model = down + [submodule] + up
 
