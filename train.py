baeb402ee62d59952fc798b8a86a62bb0691971d
Hansom99 <72391523+Hansom99@users.noreply.github.com>
2020-12-10 11:28:08 +0100

    Update train.py

--- train.py
+++ train.py
@@ -37,6 +37,7 @@ if __name__ == '__main__':
 
     
     model.addNoice2Conv()
+    exit()
 
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
         epoch_start_time = time.time()  # timer for entire epoch
